77a3c3fab54a0a43dc23676e29052b3f188d7ccb
Tim Porter <Tim.Porter@sgsi.com>
2022-04-07 16:37:40 -0700

    Add Leaflet demo.

---
+++ Client/Pages/LeafletMap.razor.js
@@ -0,0 +1,7 @@
+﻿const map = L.map('map').setView([47.608013, -122.335167], 13);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: `Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>`,
+  maxZoom: 18,
+  tileSize: 256,
+}).addTo(map);
